4f57df8d79a954d9845addfe21da58c735e5ea85
Yamlak Negash <yamlaknegash96@gmail.com>
2025-08-21 00:26:57 +0300

    Basic File Routing with landing and about page.

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function about() {
+  return (
+    <div>
+   <h1 className="text-xl font-extralight">About Page</h1>
+    </div>
+  )
+}
+
+export default about
---
+++ pages/landing.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function landing() {
+  return (
+    <div>
+      <h1 className=" text-xl font-extralight">Landing Page</h1>
+    </div>
+  )
+}
+
+export default landing
